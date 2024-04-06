17924af7dc6df0414c45bc0526dd7607364211b3
alex-aks64 <165569356+alex-aks64@users.noreply.github.com>
2024-04-06 21:38:52 +0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,28 @@
+# x = 38
+# print("дратути!")
+# if x < 0:
+#     print("меньше нуля")
+# print("дотвидание")
+
+a, b = 10, 5
+if a > b:
+    print("a > b")
+if a > b and a > 0:
+    print("успех")
+if (a > b) and (a > 0 or b < 1000):
+    print('успех1')
+
+if 5 < b and b < 10:
+    print('успех2')
+
+if "34" > "123":
+    print('успех3')
+if "123" > "12":
+    print("успех4")
+if [1,2]>[1,1]:
+    print('успех5')
+
+if '6' > str(5):
+    print("успех6")
+if '6' != str(5):
+    print('успех7')
